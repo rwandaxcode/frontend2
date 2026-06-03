8773e3ec9012e48b498f2698a5cae39d9f8fda5b
rwandaxcode <rwandaxcode@gmail.com>
2026-06-03 00:09:31 -0700

    Create Warehouse.jsx

---
+++ components/pages/Warehouse.jsx
@@ -0,0 +1,25 @@
+// src/pages/Warehouse.jsx
+
+import Navbar from "../components/Navbar"
+
+function Warehouse() {
+
+    return (
+
+        <div>
+
+            <Navbar />
+
+            <div className="p-5">
+
+                <h1 className="text-3xl font-bold">
+                    Warehouse Page
+                </h1>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Warehouse
